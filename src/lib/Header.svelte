5eaa89dc627ec93c0eb4032c2240e139167342d7
pbuterba <pbuterba.corp@gmail.com>
2024-12-07 09:46:09 -0500

    Style header

--- src/lib/Header.svelte
+++ src/lib/Header.svelte
@@ -2,3 +2,18 @@
     <h1>How Do I...?</h1>
     <h2>A one stop shop for all your questions about how to "adult"</h2>
 </main>
+<style>
+    main {
+        font-family: sans-serif;
+        text-align: center;
+        background: lightgray;
+        padding: 1vh 0;
+    }
+    h1 {
+        font-size: 3.5rem;
+    }
+    h2 {
+        font-size: 1rem;
+        font-style: italic;
+    }
+</style>
