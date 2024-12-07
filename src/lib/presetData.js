18edeb73ba9e3b38585c971694d4d953a6b0fbd1
pbuterba <pbuterba.corp@gmail.com>
2024-12-07 09:32:27 -0600

    Add category accordions

---
+++ src/lib/presetData.js
@@ -0,0 +1,30 @@
+export const categories = [
+    {
+        "name": "Cooking",
+        "color": "#FFFF00"
+    },
+    {
+        "name": "Health",
+        "color": ""
+    },
+    {
+        "name": "Finances",
+        "color": ""
+    },
+    {
+        "name": "Laundry",
+        "color": ""
+    },
+    {
+        "name": "Travel",
+        "color": ""
+    },
+    {
+        "name": "Car ownership",
+        "color": ""
+    },
+    {
+        "name": "Insurance",
+        "color": ""
+    }
+]
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,10 +1,20 @@
 <script>
     import {Accordion, AccordionHeader, AccordionBody} from 'kiwi-nl';
+    import {categories} from '$lib/presetData.js';
 </script>
 
 <main>
     <div class="container">
         <h1>Frequently Asked Questions:</h1>
+        {#each categories as category}
+            <Accordion>
+                <div slot="header">
+                    <AccordionHeader>
+                        {category.name}
+                    </AccordionHeader>
+                </div>
+            </Accordion>
+        {/each}
     </div>
 </main>
 
